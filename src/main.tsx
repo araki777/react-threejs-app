4a8ce6857752df3751eaaa358516a7033ef6732e
ma-araki <ma-araki@psc-inc.co.jp>
2023-07-07 17:37:18 +0900

    初期構築

---
+++ src/main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Canvas>
+      <Experience />
+    </Canvas>
+  </React.StrictMode>,
+)
